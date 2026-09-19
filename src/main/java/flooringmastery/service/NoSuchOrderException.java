8fcf81641ee53b49c8c32c46127e45340c65b1ed
antoniosafi <nomansafi@live.co.uk>
2026-09-19 22:15:04 +0100

    exceptions created

---
+++ src/main/java/flooringmastery/service/NoSuchOrderException.java
@@ -0,0 +1,12 @@
+package flooringmastery.service;
+
+public class NoSuchOrderException extends Exception{
+
+    public NoSuchOrderException(String message) {
+        super(message);
+    }
+
+    public NoSuchOrderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
